cb5a2555f7e053fc3b613b29e73bb79d468f45fc
RasmusJannerup <93704010+RasmusJannerup@users.noreply.github.com>
2022-06-02 21:16:53 +0200

    initialize firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,9 +5,14 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:sound_queue/app/app.dart';
 import 'package:sound_queue/bootstrap.dart';
+import 'package:sound_queue/firebase_options.dart';
 
-void main() {
-  bootstrap(App.new);
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await bootstrap(App.new);
 }
